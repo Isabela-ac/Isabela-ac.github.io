abe3fbe9254c23a9a9f4d82bad3511f64de70341
Isabela Assunção <isa.btssp2015@gmail.com>
2021-11-30 02:38:42 -0200

    Atualização main page

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ function redireciona() {
     window.open(url = 'https://github.com/Isabela-ac');
 }
 name.addEventListener('click', redireciona);
+
+/*
+h1 {
+  font-size: 45px;
+}
+
+h1:hover {
+  color: blanchedalmond;
+}
+*/
